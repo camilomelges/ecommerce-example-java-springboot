ea308428710de9713a891e60be48f3d02f68d067
reginaldo <camilo.melges@gmail.com>
2020-02-20 14:12:56 -0400

    Change onething, i dont remember

--- src/main/java/com/ecommerce/ecommerceexample/EcommerceExampleApplication.java
+++ src/main/java/com/ecommerce/ecommerceexample/EcommerceExampleApplication.java
@@ -2,8 +2,16 @@ package com.ecommerce.ecommerceexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class})
+@EnableCaching
+@ComponentScan("com.ecommerce.ecommerceexample.infra.repositories")
 public class EcommerceExampleApplication {
 
 	public static void main(String[] args) {
